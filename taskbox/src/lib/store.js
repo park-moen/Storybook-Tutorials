c842f63ebaf9d05046765dc1a76883fe57babd1a
park-moen <pmj6516@naver.com>
2024-07-04 22:43:00 +0900

    Intro to Storybook - Construct a screen

--- taskbox/src/lib/store.js
+++ taskbox/src/lib/store.js
@@ -1,4 +1,4 @@
-import { configureStore, createSlice } from '@reduxjs/toolkit';
+import { configureStore, createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 const defaultTasks = [
   { id: '1', title: 'something', state: 'TASK_INBOX' },
@@ -11,6 +11,21 @@ const TaskBoxData = {
   status: 'idle',
   error: null,
 };
+
+export const fetchTasks = createAsyncThunk('todos/fetchTodos', async () => {
+  const response = await fetch(
+    'https://jsonplaceholder.typicode.com/todos?userId=1'
+  );
+  const data = await response.json();
+  const result = data.map(task => ({
+    id: `${task.id}`,
+    title: task.title,
+    state: task.completed ? 'TASK_ARCHIVED' : 'TASK_INBOX',
+  }));
+
+  return result;
+});
+
 const TaskSlice = createSlice({
   name: 'taskbox',
   initialState: TaskBoxData,
@@ -23,6 +38,25 @@ const TaskSlice = createSlice({
         state.tasks[task].state = newTaskState;
       }
     },
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchTasks.pending, state => {
+        state.status = 'loading';
+        state.error = null;
+        state.tasks = [];
+      })
+      .addCase(fetchTasks.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.error = null;
+        // Add any fetched tasks to the array
+        state.tasks = action.payload;
+      })
+      .addCase(fetchTasks.rejected, state => {
+        state.status = 'failed';
+        state.error = 'Something went wrong';
+        state.tasks = [];
+      });
   },
 });
 
